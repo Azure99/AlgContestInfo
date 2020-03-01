e34a8c5ed658eb0270a059e521e323c254d1e03d
Azure99 <961523404@qq.com>
2020-03-01 17:26:37 +0800

    Update HttpRequester.java

--- src/main/java/com/rainng/algcontestinfo/util/HttpRequester.java
+++ src/main/java/com/rainng/algcontestinfo/util/HttpRequester.java
@@ -9,6 +9,8 @@ import java.util.Map;
 
 @Component
 public class HttpRequester {
+    private static final String HTTP_GET = "GET";
+    private static final String HTTP_POST = "POST";
     private static final int CONNECT_TIMEOUT = 10000;
     private static final int READ_TIMEOUT = 30000;
     private static final String USER_AGENT_KEY = "User-Agent";
@@ -16,37 +18,15 @@ public class HttpRequester {
 
     public String get(String address) throws IOException {
         URL url = new URL(address);
-
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setConnectTimeout(CONNECT_TIMEOUT);
-        conn.setReadTimeout(READ_TIMEOUT);
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty(USER_AGENT_KEY, USER_AGENT);
+        HttpURLConnection conn = createConnection(url, HTTP_GET);
         conn.connect();
 
-        try (InputStream inputStream = conn.getInputStream()) {
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-                StringBuilder builder = new StringBuilder();
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    builder.append(line);
-                }
-
-                return builder.toString();
-            }
-        }
+        return readDataString(conn);
     }
 
     public String post(String address, String data, Map<String, String> headers) throws IOException {
         URL url = new URL(address);
-
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setConnectTimeout(CONNECT_TIMEOUT);
-        conn.setReadTimeout(READ_TIMEOUT);
-        conn.setRequestMethod("POST");
-        conn.setDoOutput(true);
-        conn.setRequestProperty(USER_AGENT_KEY, USER_AGENT);
+        HttpURLConnection conn = createConnection(url, HTTP_POST);
         for (String key : headers.keySet()) {
             conn.setRequestProperty(key, headers.get(key));
         }
@@ -59,6 +39,23 @@ public class HttpRequester {
             }
         }
 
+        return readDataString(conn);
+    }
+
+    private HttpURLConnection createConnection(URL url, String method) throws IOException {
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setConnectTimeout(CONNECT_TIMEOUT);
+        conn.setReadTimeout(READ_TIMEOUT);
+        conn.setRequestMethod(method);
+        conn.setRequestProperty(USER_AGENT_KEY, USER_AGENT);
+        if (method.equals(HTTP_POST)) {
+            conn.setDoOutput(true);
+        }
+
+        return conn;
+    }
+
+    private String readDataString(HttpURLConnection conn) throws IOException {
         try (InputStream inputStream = conn.getInputStream()) {
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
                 StringBuilder builder = new StringBuilder();
